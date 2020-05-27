af154625f8a35ae143f30e141dda76651521a5db
Vicente <vince.geek404@gmail.com>
2020-05-26 19:22:48 -0700

    Adding the first obstacle in Level1

---
+++ Assets/SceneControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
